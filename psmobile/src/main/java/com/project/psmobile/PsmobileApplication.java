bbf445024b3cf3ce9b4e263ff0fe6303c3b73122
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-06-17 22:29:43 +0630

    start

---
+++ psmobile/src/main/java/com/project/psmobile/PsmobileApplication.java
@@ -0,0 +1,13 @@
+package com.project.psmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsmobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsmobileApplication.class, args);
+	}
+
+}
---
+++ psmobile/src/test/java/com/project/psmobile/PsmobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.psmobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsmobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
